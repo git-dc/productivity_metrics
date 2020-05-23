f36582d3a5338b5b8581220fe0b584c7c5edf1fb
Daniel Chirita <dschirita@gmail.com>
2020-05-23 14:21:43 +0400

    added data window parameter; added color query

--- calendar_proc.py
+++ calendar_proc.py
@@ -22,6 +22,22 @@ except ImportError:
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
+window_size = 60
+color_map = {
+    "1":"Lavender",
+    "2":"Sage",
+    "3":"Grape",
+    "4":"Flamingo",
+    "5":"Banana",
+    "6":"Tangerine",
+    "7":"Peacock",
+    "8":"Graphite",
+    "9":"Blueberry",
+    "10":"Basil",
+    "11":"Tomato",
+    "-":"Peacock"
+}
+
 
 def main():
     """Shows basic usage of the Google Calendar API.
@@ -50,17 +66,24 @@ def main():
 
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=10, singleEvents=True,
+    start_date = (datetime.datetime.today() - datetime.timedelta(window_size)).isoformat() + 'Z'
+    print(f'Getting the events in the {start_date} - {now} interval:')
+    events_result = service.events().list(calendarId='primary', timeMin=start_date, timeMax=now,
+                                        maxResults=2500, singleEvents=True, 
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
+    print(f"Total number of events: {len(events)}")
 
     if not events:
         print('No upcoming events found.')
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+        end = event['end'].get('dateTime', event['end'].get('date'))
+        print(start, end, event['summary'], end=" ")
+        try:
+            print(color_map[event['colorId']])
+        except:
+            print(color_map["-"])
 
 
 if __name__ == '__main__':
